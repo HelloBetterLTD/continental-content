10c146d2257a3115ec189aa84b761a593490d377
Nivanka Fonseka <nivanka@silverstripers.com>
2015-03-11 19:46:50 +0530

    DBIPCom is added in to the migrators

---
+++ code/Utils/ImportDBIPcom.php
@@ -0,0 +1,62 @@
+<?php
+class ImportDBIPcom extends BuildTask{
+	
+	protected $description = 'Import DBIPcom service to IPToLocation dataobjects from CSV';
+	protected $connection;
+
+	public function run($request) {
+
+		$strCSVPath = CONTINENTAL_CONTENT_PATH . '/code/ThirdParty/dbip-city.csv';
+
+		if(!file_exists($strCSVPath)){
+			echo "<p>I cant find the IP2LOCATION-DB.CSV file to import any data.<br>
+				Please download DB3.LITE database from <a href='http://lite.ip2location.com/'>http://lite.ip2location.com/</a>.<br>
+				NOTE: It's adviced to edit the DB to only include the countries you want to handle, it contains 2 million records!!!<br>
+				Or make a CSV contain these columns<br>
+				`IPFrom`,`IPTo`,`Country`,`Region`,`City`
+				</p>";
+			//"0.0.0.0","0.255.255.255","US","California","Los Angeles"
+		}
+		else{
+
+			if(!isset($_REQUEST['confirm'])){
+				$strLink = Director::baseURL() . 'dev/tasks/ImportDBIPcom?confirm=1';
+				echo "<p>CAUTION!!!<br>
+					Please confirm your action<br>
+					<a href='$strLink'>I confirm the action</a><br>
+					<a href='{$strLink}&emptydb=1'>I confirm the action, please empty the DB before you import</a>
+					</p>";
+			}
+
+			else{
+
+				increase_time_limit_to();
+				if(isset($_REQUEST['emptydb'])){
+					DB::query('TRUNCATE `IpToLocation`;');
+				}
+
+				$arrFields = array_keys(Config::inst()->get('IpToLocation', 'db'));
+				$handle = fopen($strCSVPath, "r");
+				if ($handle) {
+
+					while (($line = fgets($handle)) !== false) {
+						$line = str_replace('","', '___', $line);
+						$line = str_replace('"', '', $line);
+						$arrParts = Convert::raw2sql(explode("___", $line));
+						$arrParts[0] = ContinentalContentUtils::IPAddressToIPNumber($arrParts[0]);
+						$arrParts[1] = ContinentalContentUtils::IPAddressToIPNumber($arrParts[1]);
+						DB::query('INSERT INTO `IpToLocation` (`' . implode('`,`', $arrFields) .'`) VALUES (\'' . implode('\',\'', $arrParts) . '\')');
+					}
+
+					fclose($handle);
+				} else {
+					echo 'Error opening file';
+				}
+
+			}
+
+		}
+
+
+	}
+}
